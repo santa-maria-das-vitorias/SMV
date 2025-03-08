eee1760849091fa5f3b376ae0660c8ffe5e6e0a9
Pedro Laurenti <pedrolaurenti04@gmail.com>
2025-03-08 02:11:50 -0300

    FIX: Scroll

--- front-end-public/src/router/index.ts
+++ front-end-public/src/router/index.ts
@@ -10,81 +10,90 @@ declare global {
   }
 }
 
+const routes = [
+  {
+    path: '/',
+    name: 'Início',
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/sobre',
+    name: 'Sobre',
+    component: () => import('@/views/AboutView.vue')
+  },
+  {
+    path: '/padroeiros',
+    name: 'Patronos',
+    component: () => import('@/views/PadroeirosView.vue')
+  },
+  {
+    path: '/liturgia',
+    name: 'Liturgia',
+    component: () => import('@/views/LiturgiaView.vue')
+  },
+  {
+    path: '/contato',
+    name: 'Contato',
+    component: () => import('@/views/ContactView.vue')
+  },
+  {
+    path: '/politica-de-privacidade',
+    name: 'Política de Privacidade',
+    component: () => import('@/views/PoliticaView.vue')
+  },
+  {
+    path: '/carta-dos-cardeais-ottaviani-e-bacci-contra-a-promulgacao-da-missa-nova',
+    name: 'Carta dos Cardeais Ottaviani e Bacci contra a promulgação da Missa Nova',
+    component: () => import('@/views/CartaView.vue')
+  },
+  {
+    path: '/artigos',
+    name: 'Artigos',
+    component:  () => import('@/views/Articles/Index.vue'),
+    props: true,
+  },
+  {
+    path: '/projetos',
+    name: 'Projetos',
+    component:  () => import('@/views/ComingView.vue'),
+    props: true,
+  },
+  {
+    path: '/suma-teologica',
+    name: 'Suma Teologica',
+    component:  () => import('@/views/ComingView.vue'),
+    props: true,
+  },
+  {
+    path: '/fotos',
+    name: 'Fotos',
+    component:  () => import('@/views/ComingView.vue'),
+    props: true,
+  },
+  {
+    path: '/artigos/:categoria',
+    name: 'Categoria',
+    component:  () => import('@/views/Articles/CategoryView.vue'),
+    props: true,
+  },
+  {
+    path: '/artigos/:categoria/:slug',
+    name: 'Artigo',
+    component:  () => import('@/views/Articles/ArticleView.vue'),
+    props: (route: { params: { categoria: any; slug: any; }; }) => ({ categoria: route.params.categoria, slug: route.params.slug }),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Início',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/sobre',
-      name: 'Sobre',
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path: '/padroeiros',
-      name: 'Patronos',
-      component: () => import('@/views/PadroeirosView.vue')
-    },
-    {
-      path: '/liturgia',
-      name: 'Liturgia',
-      component: () => import('@/views/LiturgiaView.vue')
-    },
-    {
-      path: '/contato',
-      name: 'Contato',
-      component: () => import('@/views/ContactView.vue')
-    },
-    {
-      path: '/politica-de-privacidade',
-      name: 'Política de Privacidade',
-      component: () => import('@/views/PoliticaView.vue')
-    },
-    {
-      path: '/carta-dos-cardeais-ottaviani-e-bacci-contra-a-promulgacao-da-missa-nova',
-      name: 'Carta dos Cardeais Ottaviani e Bacci contra a promulgação da Missa Nova',
-      component: () => import('@/views/CartaView.vue')
-    },
-    {
-      path: '/artigos',
-      name: 'Artigos',
-      component:  () => import('@/views/Articles/Index.vue'),
-      props: true,
-    },
-    {
-      path: '/projetos',
-      name: 'Projetos',
-      component:  () => import('@/views/ComingView.vue'),
-      props: true,
-    },
-    {
-      path: '/suma-teologica',
-      name: 'Suma Teologica',
-      component:  () => import('@/views/ComingView.vue'),
-      props: true,
-    },
-    {
-      path: '/fotos',
-      name: 'Fotos',
-      component:  () => import('@/views/ComingView.vue'),
-      props: true,
-    },
-    {
-      path: '/artigos/:categoria',
-      name: 'Categoria',
-      component:  () => import('@/views/Articles/CategoryView.vue'),
-      props: true,
-    },
-    {
-      path: '/artigos/:categoria/:slug',
-      name: 'Artigo',
-      component:  () => import('@/views/Articles/ArticleView.vue'),
-      props: route => ({ categoria: route.params.categoria, slug: route.params.slug }),
-    },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
